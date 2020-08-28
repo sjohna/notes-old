2b66d6eae13349cb4bfbffab2470e0558af4ac3a
John A. Stevens <john@otherskylines.com>
2020-08-28 15:50:51 -0600

    GUI Hello World

---
+++ src/Notes/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Numerics;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+using ImGuiNET;
+
+namespace Notes
+{
+    class Program
+    {
+        private static Sdl2Window _window;
+        private static GraphicsDevice _gd;
+        private static CommandList _cl;
+        private static ImGuiRenderer _controller;
+
+        // UI state
+        private static Vector3 _clearColor = new Vector3(0.45f, 0.55f, 0.6f);
+
+        private static ImFontPtr customFont;
+
+        static void Main(string[] args)
+        {
+            // Create window, GraphicsDevice, and all resources necessary for the demo.
+            VeldridStartup.CreateWindowAndGraphicsDevice(
+                new WindowCreateInfo(50, 50, 1280, 720, WindowState.Normal, "Notes"),
+                new GraphicsDeviceOptions(true, null, true),
+                out _window,
+                out _gd);
+            _window.Resized += () =>
+            {
+                _gd.MainSwapchain.Resize((uint)_window.Width, (uint)_window.Height);
+                _controller.WindowResized(_window.Width, _window.Height);
+            };
+            _cl = _gd.ResourceFactory.CreateCommandList();
+            _controller = new ImGuiRenderer(_gd, _gd.MainSwapchain.Framebuffer.OutputDescription, _window.Width, _window.Height);
+
+            // Main application loop
+            while (_window.Exists)
+            {
+                InputSnapshot snapshot = _window.PumpEvents();
+                if (!_window.Exists) { break; }
+                _controller.Update(1f / 60f, snapshot); // Feed the input events to our ImGui controller, which passes them through to ImGui.
+
+                SubmitUI();
+
+                _cl.Begin();
+                _cl.SetFramebuffer(_gd.MainSwapchain.Framebuffer);
+                _cl.ClearColorTarget(0, new RgbaFloat(_clearColor.X, _clearColor.Y, _clearColor.Z, 1f));
+                _controller.Render(_gd, _cl);
+                _cl.End();
+                _gd.SubmitCommands(_cl);
+                _gd.SwapBuffers(_gd.MainSwapchain);
+            }
+
+            // Clean up Veldrid resources
+            _gd.WaitForIdle();
+            _controller.Dispose();
+            _cl.Dispose();
+            _gd.Dispose();
+        }
+
+        private static unsafe void SubmitUI()
+        {
+            {
+                ImGui.GetStyle().WindowRounding = 0;
+
+                ImGui.SetNextWindowPos(new Vector2(0, 0));
+                ImGui.SetNextWindowSize(new Vector2(_window.Width, _window.Height));
+                ImGui.Begin("", ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoDecoration);
+
+                ImGui.Text("Hello world!");
+
+                ImGui.End();
+            }
+        }
+    }
+}
