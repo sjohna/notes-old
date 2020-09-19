3c058574a7e4412e3df5156e2af3a3e7c392cf41
John A. Stevens <john@otherskylines.com>
2020-09-19 10:11:32 -0600

    Add more tests of SimpleTwoPanelUI

--- src/UIImageTests/TestSimpleTwoPanelUI.cs
+++ src/UIImageTests/TestSimpleTwoPanelUI.cs
@@ -58,5 +58,120 @@ namespace UIImageTests
             testInfo.Window.UserInterface = userInterface;
             DoTest(testInfo);
         }
+
+        [Test]
+        [TestCaseSource(nameof(TestCaseParameters))]
+        public void SingleLineInputText(string renderType, int windowWidth, int windowHeight)
+        {
+            var testName = $"{nameof(SingleLineInputText)}_{renderType.Replace(" ", "")}Renderer";
+            testInfo = new UIImageTestInfo(nameof(TestSimpleTwoPanelUI), testName, windowWidth, windowHeight);
+
+            var userInterface = new SimpleTwoPanelUI(testInfo.Window.SDLWindow);
+
+            userInterface.CurrentRenderType = renderType;
+
+            if (renderType == "AST")
+            {
+                userInterface.Renderer = new MarkdigASTRenderer();
+            }
+            else if (renderType == "Plain text")
+            {
+                userInterface.Renderer = new MarkdigPlainTextRenderer();
+            }
+
+            userInterface.InputText = "This is a single line!";
+
+            testInfo.Window.UserInterface = userInterface;
+            DoTest(testInfo);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(TestCaseParameters))]
+        public void MultipleParagraphInputText(string renderType, int windowWidth, int windowHeight)
+        {
+            var testName = $"{nameof(MultipleParagraphInputText)}_{renderType.Replace(" ", "")}Renderer";
+            testInfo = new UIImageTestInfo(nameof(TestSimpleTwoPanelUI), testName, windowWidth, windowHeight);
+
+            var userInterface = new SimpleTwoPanelUI(testInfo.Window.SDLWindow);
+
+            userInterface.CurrentRenderType = renderType;
+
+            if (renderType == "AST")
+            {
+                userInterface.Renderer = new MarkdigASTRenderer();
+            }
+            else if (renderType == "Plain text")
+            {
+                userInterface.Renderer = new MarkdigPlainTextRenderer();
+            }
+
+            userInterface.InputText = @"This is a single line!
+
+This is another line!
+
+This is a third line!";
+
+            testInfo.Window.UserInterface = userInterface;
+            DoTest(testInfo);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(TestCaseParameters))]
+        public void ListInInputText(string renderType, int windowWidth, int windowHeight)
+        {
+            var testName = $"{nameof(ListInInputText)}_{renderType.Replace(" ", "")}Renderer";
+            testInfo = new UIImageTestInfo(nameof(TestSimpleTwoPanelUI), testName, windowWidth, windowHeight);
+
+            var userInterface = new SimpleTwoPanelUI(testInfo.Window.SDLWindow);
+
+            userInterface.CurrentRenderType = renderType;
+
+            if (renderType == "AST")
+            {
+                userInterface.Renderer = new MarkdigASTRenderer();
+            }
+            else if (renderType == "Plain text")
+            {
+                userInterface.Renderer = new MarkdigPlainTextRenderer();
+            }
+
+            userInterface.InputText = @"List of things:
+ - thing one
+ - thing two
+   - thing two a
+   - thing two b
+ - thing three
+   - thing three a
+     - thing three a one";
+
+            testInfo.Window.UserInterface = userInterface;
+            DoTest(testInfo);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(TestCaseParameters))]
+        public void BoldAndItalicInputText(string renderType, int windowWidth, int windowHeight)
+        {
+            var testName = $"{nameof(BoldAndItalicInputText)}_{renderType.Replace(" ", "")}Renderer";
+            testInfo = new UIImageTestInfo(nameof(TestSimpleTwoPanelUI), testName, windowWidth, windowHeight);
+
+            var userInterface = new SimpleTwoPanelUI(testInfo.Window.SDLWindow);
+
+            userInterface.CurrentRenderType = renderType;
+
+            if (renderType == "AST")
+            {
+                userInterface.Renderer = new MarkdigASTRenderer();
+            }
+            else if (renderType == "Plain text")
+            {
+                userInterface.Renderer = new MarkdigPlainTextRenderer();
+            }
+
+            userInterface.InputText = @"This is *italic* and this is **bold** and this is ***bold italic***!";
+
+            testInfo.Window.UserInterface = userInterface;
+            DoTest(testInfo);
+        }
     }
 }
